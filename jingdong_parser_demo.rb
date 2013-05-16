0749eda6aaec301e66f63c5b8c9fd81fdd785761
LiYang <bbtfrr@gmail.com>
2013-05-16 14:55:48 +0800

    split the code into 2 parts

--- jingdong_parser_demo.rb
+++ jingdong_parser_demo.rb
@@ -1,74 +1,10 @@
+#!/usr/bin/env ruby
 # encoding: UTF-8
 
-require 'nokogiri'
-require 'open-uri'
-require 'logger'
+require_relative 'spider'
 require 'ostruct' # just for demo, inside of a db obj
 require 'pry-debugger' # for debug
 
-
-class Spider
-  
-  Log = Logger.new(STDOUT)
-  Log.formatter = proc do |severity, datetime, progname, msg|
-    "#{severity} #{datetime}: #{msg}\n"
-  end
-
-  attr_accessor :link_queue
-  attr_accessor :options
-  attr_accessor :threads
-  attr_accessor :records
-
-  DefaultOptions = {
-    verbose: false,
-    threads: 10,
-  }
-
-  def initialize options = {}
-    self.options = options.merge DefaultOptions
-
-    self.link_queue = Queue.new
-    self.threads = []
-    self.records = []
-  end
-
-  def pre_loop
-  end
-
-  def post_loop
-  end
-
-  def each_loop
-  end
-
-  def loop
-    options[:threads].times { self.threads << Thread.new { each_thread_loop }}
-    self.threads.each { |thr| thr.join }
-  end
-
-  def each_thread_loop
-    until link_queue.empty?
-      each_time
-    end
-  end
-
-  def self.parse
-    instance = self.new
-    instance.pre_loop
-    instance.loop
-    instance.post_loop
-  end
-end
-
-class Nokogiri::XML::NodeSet
-  def base_text
-    children.map{|e|e.text if e.text?}.join
-  end
-
-  def attr_text attribute_name
-    attr(attribute_name).to_s
-  end
-end
 
 class JingdongParser < Spider
 
---
+++ spider.rb
@@ -0,0 +1,70 @@
+# encoding: UTF-8
+
+require 'nokogiri'
+require 'open-uri'
+require 'logger'
+
+
+class Spider
+  
+  Log = Logger.new(STDOUT)
+  Log.formatter = proc do |severity, datetime, progname, msg|
+    "#{severity} #{datetime}: #{msg}\n"
+  end
+
+  attr_accessor :link_queue
+  attr_accessor :options
+  attr_accessor :threads
+  attr_accessor :records
+
+  DefaultOptions = {
+    verbose: false,
+    threads: 10,
+  }
+
+  def initialize options = {}
+    self.options = options.merge DefaultOptions
+
+    self.link_queue = Queue.new
+    self.threads = []
+    self.records = []
+  end
+
+  def pre_loop
+  end
+
+  def post_loop
+  end
+
+  def each_loop
+  end
+
+  def loop
+    options[:threads].times { self.threads << Thread.new { each_thread_loop }}
+    self.threads.each { |thr| thr.join }
+  end
+
+  def each_thread_loop
+    until link_queue.empty?
+      each_time
+    end
+  end
+
+  def self.parse
+    instance = self.new
+    instance.pre_loop
+    instance.loop
+    instance.post_loop
+  end
+end
+
+# a little bit nokogiri hack-in
+class Nokogiri::XML::NodeSet
+  def base_text
+    children.map{|e|e.text if e.text?}.join
+  end
+
+  def attr_text attribute_name
+    attr(attribute_name).to_s
+  end
+end
